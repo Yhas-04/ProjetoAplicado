c9221496bfaace85c30de1b8ebc295d8f4fc8e89
Miguel <migueldoiche2@gmail.com>
2026-05-28 07:47:01 -0300

    Create - Project folder

---
+++ projetoCentralizadorI/src/test/java/com/projetoInterI/projetoCentralizadorI/ProjetoCentralizadorIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoInterI.projetoCentralizadorI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCentralizadorIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
